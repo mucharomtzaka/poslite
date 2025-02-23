515a2449ac6b563cf8be9f3ccd620463b0d2c5c4
mucharomtzaka <mucharomtzaka@gmail.com>
2025-02-23 11:57:29 +0000

    change vercel php tryx

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderPrintController;
 
-
+//default route
 Route::get('/', function () {
     return redirect()->route('filament.admin.pages.dashboard');
 });
-
+// route print nota
 Route::get('/orders/{order}/print', [OrderPrintController::class, 'print'])->name('orders.print');
