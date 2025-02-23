02d0cff314e70fb9e07a1b2317256fd3325ea10b
mucharomtzaka <mucharomtzaka@gmail.com>
2025-02-23 11:55:12 +0000

    change vercel php try

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderPrintController;
 
-// Redirect root URL to Filament admin panel
+
 Route::get('/', function () {
     return redirect()->route('filament.admin.pages.dashboard');
 });
