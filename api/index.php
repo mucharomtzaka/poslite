0e6849a76bf329530f66a4e931d0f328f2463ca9
mucharomtzaka <mucharomtzaka@gmail.com>
2025-02-23 14:18:19 +0000

    composer updatex path trycx

--- api/index.php
+++ api/index.php
@@ -1,4 +1,2 @@
 <?php  
 require __DIR__ . "/../public/index.php";
-$path = env('STORAGE_PATH', storage_path('app')); // Automatically uses /tmp on Vercel
-Storage::disk('custom')->put($path . '/example.txt', 'Hello Vercel!');
