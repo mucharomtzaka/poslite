cfc7c6916d794427432617c86fc8b0ed4882361e
mucharomtzaka <mucharomtzaka@gmail.com>
2025-02-23 10:07:42 +0000

    change default route web

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderPrintController;
 
+// Redirect root URL to Filament admin panel
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('filament.admin.pages.dashboard');
 });
 
 Route::get('/orders/{order}/print', [OrderPrintController::class, 'print'])->name('orders.print');
