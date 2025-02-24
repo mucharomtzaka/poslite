e20c69d6cc76bbd56fb28ec5730507e2db5e1baf
mucharomtzaka <mucharomtzaka@gmail.com>
2025-02-24 01:19:26 +0000

    change database path app service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\File;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -22,6 +24,14 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
+
+        $dbPath = '/tmp/database.sqlite';
+
+        if (!file_exists($dbPath)) {
+            File::put($dbPath, '');
+        }
+
+        config(['database.connections.sqlite.database' => $dbPath]);
        
     }
 }
