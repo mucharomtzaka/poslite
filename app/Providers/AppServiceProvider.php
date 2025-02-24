c20a55823efbfab12da36ef37d96dd6cb8c5c2ce
mucharomtzaka <mucharomtzaka@gmail.com>
2025-02-24 02:20:30 +0000

    connecting database supaabase

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\File;
-use Illuminate\Support\Facades\Artisan;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,15 +22,6 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-
-        $dbPath = '/tmp/database.sqlite';
-
-        if (!file_exists($dbPath)) {
-            File::put($dbPath, '');
-            config(['database.connections.sqlite.database' => $dbPath]);
-             // Run migrations to create necessary tables
-            Artisan::call('migrate', ['--force' => true]);
-        }
        
     }
 }
